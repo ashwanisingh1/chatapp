7d74f239dff6eb0cd3654244e2dedc4d97610962
ashwanisingh1 <ashwanisinghnet@gmail.com>
2018-01-18 00:27:45 +0530

    updated event listeners

--- server/server.js
+++ server/server.js
@@ -11,6 +11,14 @@ app.use(express.static(publicPath));
 
 io.on('connection',(socket)=>{
   console.log('new user connected');
+  socket.emit('newMessage',{
+    from:"ashwani",
+    text:"hello",
+    createdAt:"ads"
+  });
+  socket.on('createMessage',(message)=>{
+    console.log("createMessage",message);
+  });
   socket.on('disconnect',()=>{
     console.log('user disconnected');
   });
